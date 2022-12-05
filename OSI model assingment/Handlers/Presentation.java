8f39709ace6093e90f61c68fa758248a776a1132
mesbah <mesbahmohammadi1397@gmail.com>
2022-12-05 22:11:42 +0430

    osi model

---
+++ OSI model assingment/Handlers/Presentation.java
@@ -0,0 +1,21 @@
+import Request.Data;
+
+public class Presentation implements Chain {
+    private Chain nextInChain;
+
+    @Override
+    public void setNextChain(Chain nextChain) {
+        this.nextInChain = nextChain;
+    }
+
+    @Override
+    public void process(Data request) {
+
+        if (request.getData() != null) {
+            // Do what the presentation layer does (encryption, translation)
+            nextInChain.process(request);
+        } else {
+            nextInChain.process(request);
+        }
+    }
+}
